409c62452db4432142c0c1704d87281b3efaa542
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-03-22 12:32:53 +0530

    Updated face detection

--- LiveFaceDetection/FaceDetection/livefacedetector.py
+++ LiveFaceDetection/FaceDetection/livefacedetector.py
@@ -1,3 +1,25 @@
 import numpy as np
 import cv2
 
+cap  = cv2.VideoCapture(0)
+
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalcatface.xml')
+print(face_cascade)
+while(True):
+	ret,frame = cap.read()
+	gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+	
+	faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+	for(x,y,w,h) in faces :
+		cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,255),2)
+	cv2.imshow('frame',frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+cap.release()
+cv2.destroyAllWindows()
